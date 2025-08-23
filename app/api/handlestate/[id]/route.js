ff6d5f009f5c240bc2bb0d5fbc36bdfa5c4e57f6
sahil <025.sahil@gmail.com>
2025-08-24 00:52:02 +0530

    added  theme

---
+++ app/api/handlestate/[id]/route.js
@@ -0,0 +1,15 @@
+import dbconnect from "@/app/lib/db";
+import { NextResponse } from "next/server";
+const { Taskdetail } = require("@/model/task");
+await dbconnect();
+export async function PATCH(req, { params }) {
+  const { id } = await params;
+  const state = await Taskdetail.findById(id); //gettint the state change it if want to undo the task completion feature
+  console.log("state:", state.state);
+  // https://www.mongodb.com/docs/manual/reference/operator/update/set/
+  const changesdstate = await Taskdetail.findByIdAndUpdate(id, {
+    $set: { state: 1 },
+  });
+
+  return NextResponse.json({ message: "allokay" });
+}
