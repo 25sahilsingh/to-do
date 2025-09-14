fc50a2f7ea0df2cf7a60808a8fc497595b228435
Sahil Singh <25.sahilsingh@gmail.com>
2025-09-14 07:33:04 +0530

    "ready for build"

--- app/editpage/page.js
+++ app/editpage/page.js
@@ -1,45 +1,46 @@
 "use client";
+
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 
-export default function editpage({ searchParams }) {
+function EditPageContent() {
   const [data, setdata] = useState("");
   const [temp, settemp] = useState("");
-  const useSearchParam = useSearchParams();
-  const id = useSearchParam.get("id");
-  console.log("id3", id);
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
   const handleupdate = async (e) => {
     e.preventDefault();
-    const update = await axios.put(`/api/taskchange/${id}`, {
+    await axios.put(`/api/taskchange/${id}`, {
       tasktopic: temp,
       desc: "this is desc",
-      logo: "this is logoliink",
+      logo: "this is logolink",
       state: 0,
     });
   };
+
   useEffect(() => {
     const fetchdatabyid = async (id) => {
-      console.log("id5", id);
-      const fetcheddata = await axios.get(
-        `http://localhost:3000/api/taskchange/${id}`
-      );
+      const fetcheddata = await axios.get(`/api/taskchange/${id}`);
       setdata(fetcheddata.data);
       settemp(fetcheddata.data.tasktopic);
     };
-    fetchdatabyid(id);
-  }, []);
+    if (id) fetchdatabyid(id);
+  }, [id]);
 
-  console.log("data1:", data);
   return (
     <form onSubmit={handleupdate}>
-      <input
-        value={temp}
-        onChange={(e) => {
-          settemp(e.target.value);
-        }}
-      ></input>
+      <input value={temp} onChange={(e) => settemp(e.target.value)} />
       <button type="submit">update</button>
     </form>
+  );
+}
+
+export default function EditPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPageContent />
+    </Suspense>
   );
 }
