fefe879e2e8c0efd322b5ff2dfe3771448c04afb
Sahil Singh <25.sahilsingh@gmail.com>
2025-11-22 11:02:59 +0530

    "added real time for ai suggestion"

--- app/api/ai/handlesuggestai/route.js
+++ app/api/ai/handlesuggestai/route.js
@@ -3,14 +3,15 @@ import { Client } from "@gradio/client";
 import { NextResponse } from "next/server";
 export const POST = async (req) => {
   const { task } = await req.json();
-  console.log("task:", task);
   const app = await Client.connect(
     "025sahil/8bit_stablelm-2-1_6b_forsuggestion",
     {
       hf_token: process.env.HG_TOKEN,
     }
   );
-  const result = await app.predict("/handle_task", ["10:30PM", task]);
+  let date = new Date();
+  date = date.toLocaleTimeString();
+  const result = await app.predict("/handle_task", [date, task]);
   console.log("result:", result.data[0]);
   return NextResponse.json({ suggestion: result.data[0] });
 };
